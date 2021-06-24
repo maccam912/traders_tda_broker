188edaa87e648e812514d0a69e472075b2ee6e3b
maccam912 <maccam912@gmail.com>
2021-06-24 15:58:11 -0500

    simple server to prep for auth

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,17 @@
 pub mod tda_broker;
+pub mod tda_client;
 
 #[cfg(test)]
 mod tests {
+    use crate::tda_client;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[tokio::test]
+    async fn test_server() {
+        tda_client::Client::auth_server().await;
     }
 }
---
+++ src/tda_client.rs
@@ -0,0 +1,26 @@
+use std::{convert::Infallible, net::SocketAddr};
+
+use hyper::{
+    service::{make_service_fn, service_fn},
+    Body, Request, Response, Server,
+};
+
+pub struct Client {}
+
+impl Client {
+    pub async fn auth_server() {
+        let addr = SocketAddr::from(([127, 0, 0, 1], 7043));
+        let make_svc =
+            make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(hello_world)) });
+
+        let server = Server::bind(&addr).serve(make_svc);
+
+        if let Err(e) = server.await {
+            eprintln!("server error: {}", e);
+        }
+    }
+}
+
+async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new("Hello, World".into()))
+}
