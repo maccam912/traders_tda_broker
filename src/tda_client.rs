05383614e1313bf1e12389b286ea676c85db1bf3
maccam912 <maccam912@gmail.com>
2021-06-24 16:01:06 -0500

    eventual server shutdown

--- src/tda_client.rs
+++ src/tda_client.rs
@@ -1,9 +1,10 @@
-use std::{convert::Infallible, net::SocketAddr};
+use std::{convert::Infallible, net::SocketAddr, time::Duration};
 
 use hyper::{
     service::{make_service_fn, service_fn},
     Body, Request, Response, Server,
 };
+use tokio::time::sleep;
 
 pub struct Client {}
 
@@ -13,7 +14,9 @@ impl Client {
         let make_svc =
             make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(hello_world)) });
 
-        let server = Server::bind(&addr).serve(make_svc);
+        let server = Server::bind(&addr)
+            .serve(make_svc)
+            .with_graceful_shutdown(shutdown_signal());
 
         if let Err(e) = server.await {
             eprintln!("server error: {}", e);
@@ -23,4 +26,8 @@ impl Client {
 
 async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
     Ok(Response::new("Hello, World".into()))
+}
+
+async fn shutdown_signal() {
+    sleep(Duration::from_millis(10000)).await;
 }
